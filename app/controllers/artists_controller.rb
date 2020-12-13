5f6eb19e7a2c4eda31479810e8887b730b5642c8
Zach <zbayard@gmail.com>
2020-12-13 17:16:11 -0500

    commit tree 78f833c08e2f9cace6951669f31598a74c606f82

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,38 @@
+class ArtistsController < ApplicationController
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def create
+        @artist = Artist.new(strong_params)
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update(strong_params)
+        redirect_to artist_path(@artist)
+    end
+
+
+    private
+
+    def strong_params
+        params.require(:artist).permit(:name, :bio)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/songs", to: "songs#index", as: "songs"
+  get "/songs/new", to: "songs#new", as: "new_song"
+  get "/songs/:id", to: "songs#show", as: "song"
+  get "/songs/:id/edit", to: "songs#edit", as: "edit_song"
+
+  post "/songs", to: "songs#create"
+  patch "/songs/:id", to: "songs#update" 
+
+  get "/artists", to: "artists#index", as: "artists"
+  get "/artists/new", to: "artists#new", as: "new_artist"
+  get "/artists/:id", to: "artists#show", as: "artist"
+  get "/artists/:id/edit", to: "artists#edit", as: "edit_artist"
+
+  post "/artists", to: "artists#create"
+  patch "/artists/:id", to: "artists#update" 
+
+  get "/genres", to: "genres#index", as: "genres"
+  get "/genres/new", to: "genres#new", as: "new_genre"
+  get "/genres/:id", to: "genres#show", as: "genre"
+  get "/genres/:id/edit", to: "genres#edit", as: "edit_genre"
+
+  post "/genres", to: "genres#create"
+  patch "/genres/:id", to: "genres#update" 
 end
